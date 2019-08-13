2f5da73423a81f177739ccd6480887e533edd4b3
Rajappan Arun <ARajappan@schools.nyc.gov>
2019-08-13 14:32:50 -0400

    commits

--- src/router.js
+++ src/router.js
@@ -48,6 +48,15 @@ const router =  new Router({
       ]
     },
     {
+      path: "/products",
+      name: "products",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/ProductDetail.vue")
+    },
+    {
       path: "/checkout",
       name: "checkout",
       // route level code-splitting
